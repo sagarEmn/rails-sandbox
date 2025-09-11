55ad39ccbf40be561be8ec8b9f6342dc8f57db5a
Sagar <nimokam7@gmail.com>
2025-09-11 22:19:59 +0545

    feat: add controller action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,20 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path, notice: "Product was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
 end

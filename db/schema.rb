7853d230964706501af2d7574407b127ca9f2ef7
Sagar <nimokam7@gmail.com>
2025-09-13 00:14:04 +0545

    chore: add minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_11_060512) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
